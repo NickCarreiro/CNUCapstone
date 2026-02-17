99620841b37acc78720cee55042401882dd90d7e
Nick Carreiro <nicholas.dm.carreiro@gmail.com>
2026-02-17 13:41:28 +0000

    working version before development of groups

--- start_script.sh
+++ start_script.sh
@@ -1,1 +1,10 @@
-python3 -m uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+#!/bin/bash
+
+PORT=8000
+# Ensure that the AWS Security group reflects outgoing port
+
+HOST=0.0.0.0
+#Ensure that you're pointing to an accessible endpoint (0.0.0.0 by default)
+
+python3 -m uvicorn app.main:app --reload --host 0.0.0.0 --port $PORT
+echo "Server started successfully at $PORT"
